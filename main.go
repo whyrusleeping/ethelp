009e747e1fef704287e52c0f4bd941c5750e7a7b
Jeromy <jeromyj@gmail.com>
2017-03-13 21:47:49 -0700

    add rpc subpackage

--- main.go
+++ main.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math/big"
-	"net/http"
 	"os"
-	"strings"
 	"time"
 
-	oldkeccak "github.com/ethereum/go-ethereum/crypto/sha3"
 	cli "github.com/urfave/cli"
+	util "github.com/whyrusleeping/ethelp/util"
 )
 
 var myethaddr string
@@ -34,28 +28,6 @@ func main() {
 	app.RunAndExitOnError()
 }
 
-type rpc struct {
-	JsonRPC string        `json:"jsonrpc"`
-	Method  string        `json:"method"`
-	Params  []interface{} `json:"params"`
-	Id      int           `json:"id"`
-}
-
-type response struct {
-	Error *struct {
-		Code    int
-		Message string
-		Data    interface{}
-	}
-	Result interface{}
-}
-
-type transactionParams struct {
-	From string `json:"from"`
-	To   string `json:"to,omitempty"`
-	Data string `json:"data,omitempty"`
-}
-
 var setValue = cli.Command{
 	Name: "set",
 	Action: func(c *cli.Context) error {
@@ -67,28 +39,28 @@ var setValue = cli.Command{
 		val := c.Args()[1]
 
 		method := "update(string)"
-		methash := getMethodHashThing(method)
+		methash := util.GetMethodHashThing(method)
 		fmt.Println(methash)
 
 		fmt.Printf("setting value in %s to be %s\n", caddr, val)
 
-		encval := encodeEthString(val)
+		encval := util.EncodeEthString(val)
 		loc := fmt.Sprintf("%064x", 32)
 
-		txp := &transactionParams{
+		txp := &util.TransactionParams{
 			From: myethaddr,
 			To:   caddr,
 			Data: "0x" + methash + loc + encval,
 		}
 
-		tx, err := sendTransaction(txp)
+		tx, err := util.SendTransaction(txp)
 		if err != nil {
 			return err
 		}
 		fmt.Printf("created transaction: %s\n", tx)
 
 		fmt.Println("waiting for transaction to be mined...")
-		res, err := waitForTx(tx)
+		res, err := util.WaitForTx(tx)
 		if err != nil {
 			return err
 		}
@@ -108,55 +80,27 @@ var getValue = cli.Command{
 		method := "getvalue()"
 
 		addr := c.Args().First()
-		fmt.Printf("calling '%s' on %s\n", method, addr)
+		fmt.Fprintf(os.Stderr, "calling '%s' on %s\n", method, addr)
 
-		methash := getMethodHashThing(method)
-		txp := &transactionParams{
+		methash := util.GetMethodHashThing(method)
+		txp := &util.TransactionParams{
 			From: myethaddr,
 			To:   addr,
 			Data: fmt.Sprintf("0x%s", methash),
 		}
 
-		resp, err := makeRpcCall("eth_call", []interface{}{txp})
+		resp, err := util.MakeRpcCall("eth_call", []interface{}{txp})
 		if err != nil {
 			return err
 		}
-		respval, err := decodeEthString(resp.(string)[66:])
+		respval, err := util.DecodeEthString(resp.(string)[66:])
 		if err != nil {
 			return err
 		}
 
-		fmt.Println(respval)
+		fmt.Print(respval)
 		return nil
 	},
-}
-
-func getMethodHashThing(methodsig string) string {
-	h := oldkeccak.NewKeccak256()
-	h.Write([]byte(methodsig))
-	out := h.Sum(nil)
-
-	return fmt.Sprintf("%x", out[:4])
-}
-
-func decodeEthString(s string) (string, error) {
-	raw, err := hex.DecodeString(s)
-	if err != nil {
-		return "", err
-	}
-
-	length := big.NewInt(0).SetBytes(raw[:32]).Uint64()
-	return string(raw[32 : 32+length]), nil
-}
-
-func encodeEthString(s string) string {
-	l := len(s)
-	lstr := fmt.Sprintf("%064x", l)
-	hexval := hex.EncodeToString([]byte(s))
-	padding := 64 - (len(hexval) % 64)
-	pad := strings.Repeat("0", padding)
-
-	return lstr + hexval + pad
 }
 
 var createContract = cli.Command{
@@ -171,12 +115,12 @@ var createContract = cli.Command{
 		if err != nil {
 			return err
 		}
-		txp := &transactionParams{
+		txp := &util.TransactionParams{
 			From: myethaddr,
 			Data: "0x" + string(data),
 		}
 
-		tx, err := sendTransaction(txp)
+		tx, err := util.SendTransaction(txp)
 		if err != nil {
 			return err
 		}
@@ -186,7 +130,7 @@ var createContract = cli.Command{
 		fmt.Println("waiting for contract to be mined...")
 		before := time.Now()
 
-		res, err := waitForTx(tx)
+		res, err := util.WaitForTx(tx)
 		if err != nil {
 			return err
 		}
@@ -197,65 +141,4 @@ var createContract = cli.Command{
 
 		return nil
 	},
-}
-
-func waitForTx(tx string) (interface{}, error) {
-	for {
-		res, err := makeRpcCall("eth_getTransactionReceipt", []interface{}{tx})
-		if err != nil {
-			return nil, err
-		}
-		if res != nil {
-			return res, nil
-		}
-		time.Sleep(time.Second)
-	}
-}
-
-func sendTransaction(txp *transactionParams) (string, error) {
-	res, err := makeRpcCall("eth_sendTransaction", []interface{}{txp})
-	if err != nil {
-		return "", err
-	}
-
-	return res.(string), nil
-}
-
-func makeRpcCall(method string, params []interface{}) (interface{}, error) {
-	r := &rpc{
-		JsonRPC: "2.0",
-		Method:  method,
-		Params:  params,
-		Id:      1,
-	}
-
-	marshalled, err := json.Marshal(r)
-	if err != nil {
-		return nil, err
-	}
-
-	body := bytes.NewReader(marshalled)
-
-	req, err := http.NewRequest("POST", "http://localhost:8545", body)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	var result response
-	err = json.NewDecoder(resp.Body).Decode(&result)
-	if err != nil {
-		return nil, err
-	}
-
-	if result.Error != nil {
-		return nil, fmt.Errorf("error %d: %s", result.Error.Code, result.Error.Message)
-	}
-	return result.Result, nil
 }
---
+++ util/rpc.go
@@ -0,0 +1,131 @@
+package util
+
+import (
+	"bytes"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"net/http"
+	"strings"
+	"time"
+
+	oldkeccak "github.com/ethereum/go-ethereum/crypto/sha3"
+)
+
+type Rpc struct {
+	JsonRPC string        `json:"jsonrpc"`
+	Method  string        `json:"method"`
+	Params  []interface{} `json:"params"`
+	Id      int           `json:"id"`
+}
+
+type Response struct {
+	Error *struct {
+		Code    int
+		Message string
+		Data    interface{}
+	}
+	Result interface{}
+}
+
+type TransactionParams struct {
+	From string `json:"from"`
+	To   string `json:"to,omitempty"`
+	Data string `json:"data,omitempty"`
+}
+
+func GetMethodHashThing(methodsig string) string {
+	h := oldkeccak.NewKeccak256()
+	h.Write([]byte(methodsig))
+	out := h.Sum(nil)
+
+	return fmt.Sprintf("%x", out[:4])
+}
+
+func DecodeEthString(s string) (string, error) {
+	raw, err := hex.DecodeString(s)
+	if err != nil {
+		return "", err
+	}
+
+	length := big.NewInt(0).SetBytes(raw[:32]).Uint64()
+	return string(raw[32 : 32+length]), nil
+}
+
+func EncodeEthString(s string) string {
+	l := len(s)
+	lstr := fmt.Sprintf("%064x", l)
+	hexval := hex.EncodeToString([]byte(s))
+	padding := 64 - (len(hexval) % 64)
+	pad := strings.Repeat("0", padding)
+
+	return lstr + hexval + pad
+}
+
+func WaitForTx(tx string) (interface{}, error) {
+	for {
+		res, err := MakeRpcCall("eth_getTransactionReceipt", []interface{}{tx})
+		if err != nil {
+			return nil, err
+		}
+		if res != nil {
+			return res, nil
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func SendTransaction(host string, txp *TransactionParams) (string, error) {
+	res, err := MakeRpcCallWithHost(host, "eth_sendTransaction", []interface{}{txp})
+	if err != nil {
+		return "", err
+	}
+
+	return res.(string), nil
+}
+
+const defaultApi = "http://localhost:8545"
+
+func MakeRpcCall(method string, params []interface{}) (interface{}, error) {
+	return MakeRpcCallWithHost(defaultApi, method, params)
+}
+
+func MakeRpcCallWithHost(host, method string, params []interface{}) (interface{}, error) {
+	r := &Rpc{
+		JsonRPC: "2.0",
+		Method:  method,
+		Params:  params,
+		Id:      1,
+	}
+
+	marshalled, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+
+	body := bytes.NewReader(marshalled)
+
+	req, err := http.NewRequest("POST", host, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result Response
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+
+	if result.Error != nil {
+		return nil, fmt.Errorf("error %d: %s", result.Error.Code, result.Error.Message)
+	}
+	return result.Result, nil
+}
